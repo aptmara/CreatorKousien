55f51a996d27f2e0f75cf9f8f72c04553a9662f9
IwaiShogo <sho5.i.0914@gmail.com>
2026-04-18 12:32:48 +0900

    feat: ActionPhaseStateの追加

---
+++ Assets/Scripts/Battle/Turn/States/ActionPhaseState.cs
@@ -0,0 +1,35 @@
+// ================================================================================
+// File         : ActionPhaseState.cs
+// Author       : Iwai Shogo
+//
+// Description  : 。
+// Created      : 2026-04-18
+// ================================================================================
+
+using UnityEngine;
+
+namespace CreatorKousien.Battle
+{
+    /// <summary>
+    /// アクションフェーズの処理を行います。
+    /// </summary>
+    public class ActionPhaseState : IBattlePhaseState
+    {
+        public PhaseType Type => PhaseType.Action;
+
+        public void Enter()
+        {
+            Debug.Log("--- アクションフェーズ開始 ---");
+        }
+
+        public void Update()
+        {
+            // キューの消化を TurnManager に依頼する
+        }
+
+        public void Exit()
+        {
+            Debug.Log("--- アクションフェーズ終了 ---");
+        }
+    }
+}
