426883231f6d085e79dfdb41ead4c841b32eaacd
gitfalu <t.sunny0226@gmail.com>
2026-05-10 17:33:15 +0900

    refactor : コメント追加と名前空間を適用

--- Assets/Scripts/Core/Enemy/EnemyRising.cs
+++ Assets/Scripts/Core/Enemy/EnemyRising.cs
@@ -1,43 +1,62 @@
+/**
+ * 作成：寺田晴
+ * 
+ * 内容：敵がフィールドまで上昇する処理
+ * 
+ * 
+ */
 using Game.Core.Enemy;
 using System.Collections;
 using UnityEngine;
 
-public class EnemyRising : MonoBehaviour
+namespace Game.Core.Enemy
 {
-    private EnemyController _controller;
-    private Vector3 _targetPosition;
-
-    [Tooltip("上昇にかかる時間")]
-    [SerializeField] private float _riseDuration = 1.5f;
-    [SerializeField] private AnimationCurve _riseCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
-
-    public void StartRise(EnemyController controller,Vector3 targetPos,float startYOffset)
+    /// <summary>
+    /// 敵の上昇処理を行う
+    /// </summary>
+    public class EnemyRising : MonoBehaviour
     {
-        _controller = controller;
-        _targetPosition = targetPos;
+        private EnemyController _controller;
+        //　敵の目標地点
+        private Vector3 _targetPosition;
 
-        transform.position = _targetPosition + Vector3.down * startYOffset;
+        [Tooltip("上昇にかかる時間(調整中)")]
+        [SerializeField] private float _riseDuration = 1.5f;
+        [Tooltip("上昇の際の動き(調整中)")]
+        [SerializeField] private AnimationCurve _riseCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
 
-        StartCoroutine(RiseRoutine());
-    }
-
-    private IEnumerator RiseRoutine()
-    {
-        Vector3 startPos = transform.position;
-        float elapsed = 0.0f;
-
-        while (elapsed < _riseDuration)
+        public void StartRise(EnemyController controller,Vector3 targetPos,float startYOffset)
         {
-            elapsed += Time.deltaTime;
-            float t = elapsed / _riseDuration;
-            float curveT = _riseCurve.Evaluate(t);
-
-            transform.position = Vector3.Lerp(startPos, _targetPosition, curveT);
-            yield return null;
+            // コントローラーと上昇目標の設定
+            _controller = controller;
+            _targetPosition = targetPos;
+            // 上昇目標と目標までの距離から初期位置設定
+            transform.position = _targetPosition + Vector3.down * startYOffset;
+            // 上昇開始
+            StartCoroutine(RiseRoutine());
         }
 
-        transform.position = _targetPosition;
+        private IEnumerator RiseRoutine()
+        {
+            Vector3 startPos = transform.position;
+            float elapsed = 0.0f;
+            // カーブも考慮した目標地点までの滑らかな動き
+            while (elapsed < _riseDuration)
+            {
+                // 以下なぜかインスペクターの値が反映されない
+                //-カーブの値を取得し、割合を計算
+                elapsed += Time.deltaTime;
+                float t = elapsed / _riseDuration;
+                float curveT = _riseCurve.Evaluate(t);
+                //-イージングにより位置の更新
+                transform.position = Vector3.Lerp(startPos, _targetPosition, curveT);
+                yield return null;
+            }
+            //-イージング処理完了後目標地点に位置を補正
+            transform.position = _targetPosition;
 
-        Debug.Log("[Rising] 到着");
-    }
+            Debug.Log("[Rising] 到着");
+        }
+}
+
 }
--- Assets/Scripts/Core/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Core/Enemy/EnemySpawner.cs
@@ -1,72 +1,102 @@
+/**
+ * 作成：寺田晴
+ * 
+ * 内容：敵を生成する(現在はデバッグキー込み)
+ * 
+ */
 using Game.Core.Enemy;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Editor;
 
-public class EnemySpawner : MonoBehaviour
+
+namespace Game.Core.Enemy
 {
-    [Header("DebugInput設定")]
-    [SerializeField] private InputAction _spawnAction;
-
-    [Header("生成する敵の設定")]
-    [SerializeField] private GameObject _enemyPrefab;
-    [SerializeField] private EnemyDefinition _definition;
-    [Tooltip("生成する範囲(2D:横幅*奥行)")]
-    [SerializeField] private Vector2 _rangeSize;
-
-    [Header("出現位置の設定")]
-    [Tooltip("最終目標地点")]
-    [SerializeField] private Transform _spawnBasePoint;
-    [Tooltip("どのくらい下から出現させるか")]
-    [SerializeField] private float _undergroundOffset = 10.0f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void OnEnable()
+    /// <summary>
+    /// 敵のスポーンを行う
+    /// 生成と同時に上昇処理も行う
+    /// </summary>
+    public class EnemySpawner : MonoBehaviour
     {
-        _spawnAction.Enable();
-        _spawnAction.performed += OnSpawnPerformed;
+        [Header("DebugInput設定")]
+        [SerializeField] private InputAction _spawnAction;
+
+        [Header("生成する敵の設定")]
+        [Tooltip("生成する敵のプレハブ")]
+        [SerializeField] private GameObject _enemyPrefab;
+        [Tooltip("生成する敵のデータ")]
+        [SerializeField] private EnemyDefinition _definition;
+        [Tooltip("生成する範囲(2D:横幅*奥行)")]
+        [SerializeField] private Vector2 _rangeSize;
+
+        [Header("出現位置の設定")]
+        [Tooltip("最終目標地点")]
+        [SerializeField] private Transform _spawnBasePoint;
+        [Tooltip("どのくらい下から出現させるか")]
+        [SerializeField] private float _undergroundOffset = 10.0f;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void OnEnable()
+        {
+            // デバッグインプットを行うための登録
+            _spawnAction.Enable();
+            _spawnAction.performed += OnSpawnPerformed;
+        }
+
+        private void OnDisable()
+        {
+            // デバッグインプットの登録解除
+            _spawnAction.performed -= OnSpawnPerformed;
+            _spawnAction.Disable();
+        }
+
+        /// <summary>
+        /// デバッグインプットにより行う関数
+        /// </summary>
+        /// <param name="context"></param>
+        private void OnSpawnPerformed(InputAction.CallbackContext context)
+        {
+            SpawnEnemy();
+        }
+
+        /// <summary>
+        /// 敵の生成
+        /// </summary>
+        private void SpawnEnemy()
+        {
+            if (_enemyPrefab == null) return;
+
+            // 目標位置計算
+            Vector3 spawnPos = _spawnBasePoint.position;
+            float randomX = Random.Range(spawnPos.x - _rangeSize.x / 2f, spawnPos.x + _rangeSize.x / 2f);
+            float randomZ = Random.Range(spawnPos.z - _rangeSize.y / 2f, spawnPos.z + _rangeSize.y / 2f);
+            Vector3 TargetPos = new Vector3(randomX,spawnPos.y,randomZ);
+
+            // プレハブから敵生成
+            GameObject enemyGo = Instantiate(_enemyPrefab);
+
+            // 敵の初期化
+            var controller = enemyGo.GetComponent<EnemyController>();
+            var rising = enemyGo.GetComponent<EnemyRising>();// 上昇
+            if (rising != null) rising = enemyGo.AddComponent<EnemyRising>();
+
+            controller.Initialize(_definition);
+            // 上昇処理の開始
+            rising.StartRise(controller, TargetPos, _undergroundOffset);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            // 生成位置と開始位置の可視化
+            Gizmos.color = Color.cyan;
+            Vector3 size = new Vector3(_rangeSize.x, 0.1f, _rangeSize.y);
+            Gizmos.DrawWireCube(_spawnBasePoint.position, size);
+
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireCube(_spawnBasePoint.position + Vector3.down * _undergroundOffset, size);
+        }
+
+
     }
-
-    private void OnDisable()
-    {
-        _spawnAction.performed -= OnSpawnPerformed;
-        _spawnAction.Disable();
-    }
-
-    private void OnSpawnPerformed(InputAction.CallbackContext context)
-    {
-        SpawnEnemy();
-    }
-
-    private void SpawnEnemy()
-    {
-        if (_enemyPrefab == null) return;
-
-        // 目標位置計算
-        Vector3 spawnPos = _spawnBasePoint.position;
-        float randomX = Random.Range(spawnPos.x - _rangeSize.x / 2f, spawnPos.x + _rangeSize.x / 2f);
-        float randomZ = Random.Range(spawnPos.z - _rangeSize.y / 2f, spawnPos.z + _rangeSize.y / 2f);
-        Vector3 TargetPos = new Vector3(randomX,spawnPos.y,randomZ);
-
-        GameObject enemyGo = Instantiate(_enemyPrefab);
-
-        var controller = enemyGo.GetComponent<EnemyController>();
-        var rising = enemyGo.GetComponent<EnemyRising>();// 上昇
-        if (rising != null) rising = enemyGo.AddComponent<EnemyRising>();
-
-        controller.Initialize(_definition);
-        rising.StartRise(controller, TargetPos, _undergroundOffset);
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.cyan;
-        Vector3 size = new Vector3(_rangeSize.x, 0.1f, _rangeSize.y);
-        Gizmos.DrawWireCube(_spawnBasePoint.position, size);
-
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawWireCube(_spawnBasePoint.position + Vector3.down * _undergroundOffset, size);
-    }
-
 
 }
