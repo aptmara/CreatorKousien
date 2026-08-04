61b80a65b0be97bf7adb0e7436f5c3c2615d1ee3
gitfalu <t.sunny0226@gmail.com>
2026-08-04 13:10:58 +0900

    feature : 陰を付けてBPMに動きを合わせられるように実装変更

--- Assets/Scripts/Core/Management/SoundManager.cs
+++ Assets/Scripts/Core/Management/SoundManager.cs
@@ -23,6 +23,12 @@ public class SoundManager : MonoBehaviour
     private float _erapsedTime;
     [SerializeField] private float _duration = 1.0f;
 
+    [Header("==== 揺れのBPM対応 ====")]
+    [SerializeField, Tooltip("BPMに合わせたい揺れるマテリアル")]
+    private List<Material> _swayMaterial;
+    private SoundData.AudioData _currentBGM;
+    private int _bpm = 100;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -40,19 +46,39 @@ public class SoundManager : MonoBehaviour
             _erapsedTime += Time.unscaledDeltaTime;
         }
         _playVolume = Mathf.Lerp(_audioSrc.volume, _playVolume, _erapsedTime / _duration);
+
+        double beat = 1.0f;
+        if (!_audioSrc.isPlaying)
+        {
+            beat = Time.time * (float)_bpm / 60.0f;
+        }
+        else
+        {
+            beat = _audioSrc.time * _currentBGM.bpm / 60.0f;
+        }
+        Debug.Log(beat);
+        foreach(var mat in _swayMaterial)
+        {
+            mat.SetFloat("_Beat", (float)beat);
+        }
     }
 
     public void PlaySE(string name,float volume = 1.0f)
     {
-        var se = _soundData.SEDataList.Find(x => x.Name == name).AudioClip;
+        var SEData = _soundData.SEDataList.Find(x => x.Name == name);
+        
         Debug.Log("[SoundManager]PlaySE : " + name);
-        _audioSrc.PlayOneShot(se,volume);
+        _audioSrc.PlayOneShot(SEData.AudioClip,volume);
     }
 
     public void PlayBGM(string name)
     {
         if (_audioSrc == null) return;
-        var bgm = _soundData.BGMDataList.Find(x => x.Name == name).AudioClip;
+        var BGMData = _soundData.BGMDataList.Find(x => x.Name == name);
+        
+        var bgm = BGMData.AudioClip;
+
+        _currentBGM = BGMData;
         _audioSrc.clip = bgm;
         _audioSrc.loop = true;
         _audioSrc.Play();
