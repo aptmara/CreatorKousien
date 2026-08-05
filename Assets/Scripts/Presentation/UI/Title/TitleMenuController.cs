3caed0e285b800d1e64ea2b0c7799453168c5bb7
HuTao662 <238139190+OnionRingBurger@users.noreply.github.com>
2026-08-05 19:29:45 +0900

    fix/Title側調整

--- Assets/Scripts/Presentation/UI/Title/TitleMenuController.cs
+++ Assets/Scripts/Presentation/UI/Title/TitleMenuController.cs
@@ -23,7 +23,7 @@ namespace Game.Presentation.UI.Title
         [SerializeField] private Button _startButton;
 
         [Header("--- 遷移先のシーン ---")]
-        [SerializeField] private string _loadingSceneName = "Loading";
+        [SerializeField] private string _selectSceneName = "StageSelect";
 
         public void OnEnable()
         {
@@ -37,8 +37,8 @@ namespace Game.Presentation.UI.Title
         /// </summary>
         public void OnClickNewGame()
         {
-            Debug.Log("New Game が押されたぜよ。シーン遷移: " + _loadingSceneName);
-            SceneManager.LoadScene(_loadingSceneName);
+            Debug.Log("New Game が押されたぜよ。シーン遷移: " + _selectSceneName);
+            SceneManager.LoadScene(_selectSceneName);
         }
 
         /// <summary>
